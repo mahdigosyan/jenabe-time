9edb5745fda5d39b41be5abf9215b2eeaedb1afa
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-08 07:15:59 +0330

    Update electron.js

--- electron.js
+++ electron.js
@@ -9,4 +9,15 @@ const {
   } = require("electron");
   const Store = require("electron-store");
   const isDev = require("electron-is-dev");
+// ---------------------------
+  const path = require("path");
+  const { version } = require("../package.json");
+  
+  const store = new Store();  
+
+global.notificationSettings = {
+    resetNotification: store.get("reset") || true,
+    reminderNotification: store.get("reminder") || "hour"
+  };
+
   
