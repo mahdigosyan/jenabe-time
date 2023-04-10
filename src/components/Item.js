3b2f7e32c02637136d635d72a6b63c0e4f9f5ae0
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-10 23:54:08 +0330

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,3 +2,19 @@ import React from "react";
 import { useAppReducer } from "../AppContext";
 import styles from "./Item.module.scss";
 
+// Individual todo item
+function Item({ item }) {
+    const dispatch = useAppReducer();
+    let text = item.text;
+    let paused = item.status === "paused";
+    let completed = item.status === "completed";
+    
+    function deleteItem() {
+        dispatch({ type: "DELETE_ITEM", item });
+      }
+
+      function pauseItem() {
+        const pausedItem = { ...item, status: "paused" };
+        dispatch({ type: "UPDATE_ITEM", item: pausedItem });
+      }
+      
