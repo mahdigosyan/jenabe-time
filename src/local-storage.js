a3dfb6f27e080edaf8b6c1846398349320af79e0
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-11 06:00:02 +0330

    Update local-storage.js

--- src/local-storage.js
+++ src/local-storage.js
@@ -10,4 +10,12 @@ export const loadState = () => {
     }
   };
 
+  export const saveState = state => {
+    try {
+      const serializedState = JSON.stringify(state);
+      localStorage.setItem("state", serializedState);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   
