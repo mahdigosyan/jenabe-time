0370bda073f7da076c968c03babf168d51fa0fc0
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-11 05:47:15 +0330

    Update TodoDate.js

--- src/components/TodoDate.js
+++ src/components/TodoDate.js
@@ -4,3 +4,26 @@ import useDateCheck from "../hooks/useDateCheck";
 import useReminderNotification from "../hooks/useReminderNotification";
 import styles from "./TodoDate.module.scss";
 
+function TodoDate() {
+    const { date } = useAppState();
+  
+    useDateCheck();
+    useReminderNotification();
+
+    return (
+        <div className={styles.date}>
+          <div className={styles.calendar}>
+            <div className={styles.day}>{date.dayDisplay}</div>
+            <div className={styles.my}>
+              <div className={styles.month}>{date.monthDisplay}</div>
+              <div className={styles.year}>{date.year}</div>
+            </div>
+          </div>
+          <div className="today">{date.weekday}</div>
+        </div>
+      );
+    }
+
+    export default TodoDate;
+
+    
