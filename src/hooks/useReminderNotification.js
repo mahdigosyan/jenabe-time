2a992aba372c2ef0dae46587935fc8db34ed4b14
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-11 05:51:09 +0330

    Update useReminderNotification.js

--- src/hooks/useReminderNotification.js
+++ src/hooks/useReminderNotification.js
@@ -2,3 +2,22 @@ import { useEffect } from "react";
 import { remote } from "electron";
 import { useItems } from "../AppContext";
 
+function getTimeCondition(nd) {
+    let condition = false;
+
+    switch (remote.getGlobal("notificationSettings").reminderNotification) {
+        case "hour":
+          condition = nd.getMinutes() === 0 && nd.getSeconds() === 0;
+          break;
+        case "halfhour":
+          condition = nd.getMinutes() % 30 === 0 && nd.getSeconds() === 0;
+          break;
+        case "quarterhour":
+          condition = nd.getMinutes() % 15 === 0 && nd.getSeconds() === 0;
+          break;
+        default:
+          break;
+      }
+
+      return condition;
+}
