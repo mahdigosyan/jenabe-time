da8126974c14abadcada837eb0a16c1c690f36e2
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-11 05:57:11 +0330

    Update AppContext.js

--- src/AppContext.js
+++ src/AppContext.js
@@ -24,3 +24,55 @@ export function useAppReducer() {
   return { pending, paused, completed };
 }
 
+
+const appStateReducer = (state, action) => {
+    let nd = new Date();
+  
+    let currentDate = {
+      day: format(nd, "dd"),
+      dayDisplay: format(nd, "d"),
+      month: format(nd, "MM"),
+      monthDisplay: format(nd, "MMM"),
+      year: format(nd, "y"),
+      weekday: format(nd, "EEEE")
+    };
+  
+    switch (action.type) {
+      case "ADD_ITEM": {
+        const newState = { ...state, items: state.items.concat(action.item) };
+        saveState(newState);
+        return newState;
+      }
+      case "UPDATE_ITEM": {
+        const newItems = state.items.map(i => {
+          if (i.key === action.item.key) {
+            return Object.assign({}, i, {
+              status: action.item.status
+            });
+          }
+          return i;
+        });
+        const newState = { ...state, items: newItems };
+        saveState(newState);
+        return newState;
+      }
+      case "DELETE_ITEM": {
+        const newState = {
+          ...state,
+          items: state.items.filter(item => item.key !== action.item.key)
+        };
+        saveState(newState);
+        return newState;
+      }
+      case "RESET_ALL": {
+        const newItems = state.items
+          .filter(item => item.status !== "completed")
+          .map(i => {
+            if (i.status === "paused") {
+              return Object.assign({}, i, {
+                status: "pending"
+              });
+            }
+            return i;
+          });
+          
