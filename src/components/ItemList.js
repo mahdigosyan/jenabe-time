7d148a63fb0b1b8eb0cf770430652ddf9348d678
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-11 05:40:33 +0330

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -34,4 +34,50 @@ function ItemList() {
             <img src={alldone} alt="Nothing to do!" />
           </div>
         )}
-        
+        <Accordion collapsible multiple>
+        {paused.length > 0 && (
+          <AccordionItem>
+            <AccordionButton className={styles.toggle}>
+              <img src={arrow} alt="Do Later Toggle" />
+              <span>Do Later</span>
+            </AccordionButton>
+            <AccordionPanel className={styles.panel}>
+              {paused &&
+                paused.map(item => {
+                  return <Item item={item} key={item.key} />;
+                })}
+            </AccordionPanel>
+          </AccordionItem>
+        )}
+        {completed.length > 0 && (
+          <AccordionItem>
+            <AccordionButton className={styles.toggle}>
+              <img src={arrow} alt="Completed Toggle" /> <span>Completed</span>
+            </AccordionButton>
+            <AccordionPanel className={styles.panel}>
+              {completed &&
+                completed.map(item => {
+                  return <Item item={item} key={item.key} />;
+                })}
+            </AccordionPanel>
+          </AccordionItem>
+        )}
+      </Accordion>
+
+      {(completed.length > 0 || paused.length > 0) && (
+        <div className={styles.reset}>
+          <button
+            onClick={() => {
+              dispatch({ type: "RESET_ALL" });
+            }}
+          >
+            reset progress
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ItemList;
+
