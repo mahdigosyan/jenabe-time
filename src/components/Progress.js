e62ac45a9e5550e4050078530ad262ca58aa9ef6
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-11 05:44:28 +0330

    Update Progress.js

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -3,3 +3,17 @@ import { useItems, useAppState } from "../AppContext";
 
 import styles from "./Progress.module.scss";
 
+
+function Progress() {
+    const totalAmount = useAppState().items.length;
+    const { paused, completed } = useItems();
+    const completedAmount = completed.length;
+    const pausedAmount = paused.length;
+
+    let completedPercentage = completedAmount / totalAmount;
+  let pausedPercentage = pausedAmount / totalAmount + completedPercentage;
+
+  if (isNaN(completedPercentage)) {
+    completedPercentage = 0;
+  }
+  
