6c013dcc2e3436bedbe0dc8975d0ac1cd728d97f
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-11 05:59:47 +0330

    Create local-storage.js

---
+++ src/local-storage.js
@@ -0,0 +1,13 @@
+export const loadState = () => {
+    try {
+      const serializedState = localStorage.getItem("state");
+      if (serializedState === null) {
+        return undefined;
+      }
+      return JSON.parse(serializedState);
+    } catch (err) {
+      return undefined;
+    }
+  };
+
+  
