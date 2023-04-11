9984b59775a6f77b6702cf971b157e865286c919
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-11 05:56:11 +0330

    Update AppContext.js

--- src/AppContext.js
+++ src/AppContext.js
@@ -2,3 +2,10 @@ import React, { createContext, useContext, useReducer } from "react";
 import { loadState, saveState } from "./local-storage";
 import { format } from "date-fns";
 
+
+export const AppContext = createContext();
+
+export function useAppState() {
+  return useContext(AppContext)[0];
+}
+
